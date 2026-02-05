88ab95fc09a0f34401b8804a2d64347711928957
codeHasib <mohammadhasib9827@gmail.com>
2026-02-06 00:27:10 +0600

    add all functionality for withdraw, deposit and send money

--- script/script.js
+++ script/script.js
@@ -11,67 +11,150 @@ const userAmountWarning = document.querySelector(".amount-msg");
 const appDisplay = document.querySelectorAll(".app");
 const userInfoSubmitBtn = document.querySelector(".user-info-submit");
 const userNameDisplay = document.querySelector(".userNameDisplay");
-const userAmountDisplay = document.querySelector(".userAmountDisplay");
+const userAmountDisplay = document.querySelectorAll(".userAmountDisplay");
+function render() {
+  userNameDisplay.textContent = `Hi, ${appState.userName}`;
+  userAmountDisplay.forEach((display) => {
+    display.textContent = `$${appState.userAmount}`;
+  });
+}
+function showApp() {
+  appDisplay.forEach((section) => {
+    section.style.display = "block";
+  });
+}
+function hideApp() {
+  appDisplay.forEach((section) => {
+    section.style.display = "none";
+  });
+}
 userInfoSubmitBtn.addEventListener("click", () => {
   const userNameInputValue = userNameInput.value.trim();
-  if (userNameInputValue.length === 0 && userAmountInput.value.length === 0) {
+  const userAmountInputValue = Number(userAmountInput.value);
+  if (userNameInputValue.length === 0 && userAmountInputValue === 0) {
     userNameWarning.textContent = "Please Provide Your Name";
     userAmountWarning.textContent = "Please Provide Your Amount";
   } else if (userNameInputValue.length < 3) {
     userNameWarning.textContent = "Name Is Too Short";
-  } else if (userAmountInput.value < 0) {
+  } else if (userAmountInputValue < 0) {
     userAmountWarning.textContent = "Amount Must Be Greater then 0";
   } else {
     appState.userName = userNameInputValue;
-    appState.userAmount = userAmountInput.value;
-    userNameDisplay.textContent = `Hi, ${appState.userName}`;
-    userAmountDisplay.textContent = `$${appState.userAmount}`;
+    appState.userAmount = userAmountInputValue;
+    showApp();
+    render();
+    checkHistory();
     userInfoInterface.style.display = "none";
-    appDisplay.forEach((section) => {
-      section.style.display = "block";
-    });
   }
 });
+
 const deposit = document.querySelector(".deposit");
 const depositPage = document.querySelector("#depositPage");
 const depositBtn = document.querySelector(".depositBtn");
 const depositInput = document.querySelector(".depositInput");
+const depoWarnMsg = document.querySelector(".depoWarnMsg");
 deposit.addEventListener("click", () => {
-    depositPage.style.display = "block";
-    appDisplay.forEach((section) => {
-        section.style.display = "none";
-    });
+  depositPage.style.display = "block";
+  render();
+  hideApp();
 });
-depositBtn.addEventListener("click", ()=> {
+depositBtn.addEventListener("click", () => {
+  const amount = Number(depositInput.value);
+  if (amount > 0) {
+    appState.userAmount += amount;
+    render();
+    appState.transactions.push({
+      type: "Deposit",
+      amount: amount,
+      time: new Date().toLocaleString(),
+    });
+    checkHistory();
     depositInput.value = "";
-    appState.userAmount+=depositInput.value;
-    userAmountDisplay.textContent = appState.userAmount;
+  } else {
+    depoWarnMsg.textContent = "Invalid input";
+  }
 });
 const send = document.querySelector(".send");
+const sendInput = document.querySelector(".sendMoneyInput");
+const sendBtn = document.querySelector(".sendMoneyBtn");
 const sendMoneyPage = document.querySelector("#sendMoneyPage");
+const sendWarnMsg = document.querySelector(".sendWarnMsg");
 send.addEventListener("click", () => {
-    sendMoneyPage.style.display = "block";
-    appDisplay.forEach((section) => {
-        section.style.display = "none";
+  sendMoneyPage.style.display = "block";
+  hideApp();
+});
+sendBtn.addEventListener("click", () => {
+  const amount = Number(sendInput.value);
+  if (amount <= appState.userAmount && amount > 0) {
+    appState.userAmount -= amount;
+    render();
+    appState.transactions.push({
+      type: "Send Money",
+      amount: amount,
+      time: new Date().toLocaleString(),
     });
+    checkHistory();
+    sendInput.value = "";
+  } else {
+    sendWarnMsg.textContent = "Insufficient Balance or Invalid Input";
+  }
 });
 const withdraw = document.querySelector(".withdraw");
+const withdrawBtn = document.querySelector(".withdrawBtn");
+const withdrawInput = document.querySelector(".withdrawInput");
 const withdrawPage = document.querySelector("#withdrawPage");
+const withWarnMsg = document.querySelector(".withWarnMsg");
 withdraw.addEventListener("click", () => {
-    withdrawPage.style.display = "block";
-    appDisplay.forEach((section) => {
-        section.style.display = "none";
+  withdrawPage.style.display = "block";
+  hideApp();
+});
+withdrawBtn.addEventListener("click", () => {
+  const amount = Number(sendInput.value);
+  if (amount <= appState.userAmount && amount > 0) {
+    appState.userAmount -= amount;
+    render();
+    appState.transactions.push({
+      type: "Withdraw",
+      amount: amount,
+      time: new Date().toLocaleString(),
     });
+    checkHistory();
+    withdrawInput.value = "";
+  } else {
+    withWarnMsg.textContent = "Insufficient Balance or Invalid Input";
+  }
 });
 const backBtn = document.querySelectorAll(".back-btn");
 const transactionPages = document.querySelectorAll(".transaction-pages");
 backBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        transactionPages.forEach((section) => {
-            section.style.display = "none";
-        });
-        appDisplay.forEach((section) => {
-            section.style.display = "block";
-        });
+  btn.addEventListener("click", () => {
+    transactionPages.forEach((section) => {
+      section.style.display = "none";
     });
+    showApp();
+  });
 });
+const historyDiv = document.querySelector(".historyDiv");
+function checkHistory() {
+  if (appState.transactions.length > 0) {
+    historyDiv.innerHTML = "";
+    appState.transactions.forEach((trans) => {
+      let div = document.createElement("div");
+      let h2 = document.createElement("h2");
+      h2.textContent = trans.type;
+      div.append(h2);
+      let h3 = document.createElement("h3");
+      h3.textContent = trans.amount;
+      div.append(h3);
+      let h4 = document.createElement("h4");
+      h4.textContent = trans.time;
+      div.append(h4);
+      historyDiv.append(div);
+    });
+  } else {
+    let h2 = document.createElement("h2");
+    h2.style.textAlign = "center";
+    h2.textContent = "No History";
+    historyDiv.append(h2);
+  }
+}
